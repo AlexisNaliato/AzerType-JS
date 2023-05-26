42abeea2be782336f5101851c462d35d9f94e961
Alexis <naliatoalexis@gmail.com>
2023-05-26 15:52:54 +0200

    Changements dans la liste Mots et Phrases

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
 // Tableau de mot et phrases pour le jeu .
-const listePhrases = ["Pas de panique !", "Merci pour le poisson", "Elden ring GOTY"]
-const listeMots = ["Cachalot", "Pétunia", "Serviette","Boris"]
+const listePhrases = ["Pas de panique !", "Merci pour le poisson", "Elden ring GOTY", "Je peux pas j'ai piscine", "Si seulement c'était vrai"]
+const listeMots = ["Livre", "Pétunia", "Serviette","Voiture", "Guitare", "Dictionnaire", "Télévision", "Radiateur", "Pomme"]
